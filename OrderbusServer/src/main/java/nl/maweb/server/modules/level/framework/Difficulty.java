e817ebc0b4f7c2e51f2266d7c55b2b008f5e1ec9
Spraxs <jaimy.putter@outlook.com>
2018-12-10 20:56:09 +0100

    See last commit

--- OrderbusServer/src/main/java/nl/maweb/server/modules/level/framework/Difficulty.java
+++ OrderbusServer/src/main/java/nl/maweb/server/modules/level/framework/Difficulty.java
@@ -1,4 +1,13 @@
 package nl.maweb.server.modules.level.framework;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor @Getter
 public enum Difficulty {
+
+    NORMAL(0),
+    HARD(1);
+
+    private int id;
 }
